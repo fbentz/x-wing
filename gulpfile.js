b213a51b6b26906585e58c2e40034183c84e8fd2
Fabrice Bentz <fabrice@weeple.fr>
2015-03-29 21:26:56 +0200

    Add watchify task and html asset task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var gulp = require('gulp');
+var util = require('gulp-util')
 var path = require('path');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var babelify = require('babelify');
+var watchify = require('watchify');
 
 /**
  * Configuration options
@@ -21,14 +23,29 @@ var options = {
       'react/addons',
       'material-ui'
     ]
-  }
+  },
+  htmlAssets: [
+    'src/index.html'
+  ]
 };
+
+/**
+ * Assets tasks
+ */
+gulp.task('assets:html', function() {
+  return gulp.src(options.htmlAssets)
+    .pipe(gulp.dest(options.target));
+});
+
 
 /**
  * JS Tasks
  */
 gulp.task('js', ['bundle:app']);
 
+/**
+ * Bundle Vendors
+ */
 gulp.task('bundle:vendors', function() {
   return browserify()
     .require(options.vendors.modules)
@@ -37,11 +54,41 @@ gulp.task('bundle:vendors', function() {
     .pipe(gulp.dest(path.join(options.target,'js')));
 });
 
+/**
+ * Bundle app
+ */
 gulp.task('bundle:app', ['bundle:vendors'], function() {
   return browserify(options.app.src)
     .external(options.vendors.modules)
     .transform(babelify)
     .bundle()
     .pipe(source(options.app.target))
-    .pipe(gulp.dest(path.join(options.target, 'js')));
+    .pipe(gulp.dest(path.join(options.target,'js')));
 });
+
+/**
+ * Watchify
+ */
+gulp.task('watchify', function() {
+  var bundle = browserify(options.app.src, watchify.args)
+    .external(options.vendors.modules)
+    .transform(babelify);
+
+  function updateBundle(watch) {
+    return watch.bundle()
+      .pipe(source(options.app.target))
+      .pipe(gulp.dest(path.join(options.target,'js')));
+  }
+
+  var watcher = watchify(bundle);
+
+  watcher.on('update', function() {
+    updateBundle(watcher);
+  });
+
+  watcher.on('log', function(msg) {
+    util.log(util.colors.green(msg));
+  });
+
+  return updateBundle(watcher);
+})
