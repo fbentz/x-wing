7d8572fe9c95bb39d625acf4b88f08584f7b9f05
Fabrice Bentz <fabrice@weeple.fr>
2015-03-31 00:18:46 +0200

    Add chai as promised and update karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'browserify', 'chai'],
+    frameworks: ['mocha', 'browserify', 'chai', 'chai-as-promised'],
 
 
     // list of files / patterns to load in the browser
