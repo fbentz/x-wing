1221b0c30bc60b3c0900c224f9228a322025c3d3
Fabrice Bentz <fabrice@weeple.fr>
2015-03-30 22:53:24 +0200

    Add fixtures pilot

---
+++ src/test/fixtures/pilot.js
@@ -0,0 +1,20 @@
+'use strict';
+export const Pilot = {
+  shipType: 'xwing',
+  name: 'Red squadron Pilot',
+  skill: 2,
+  faction: 'Rebel',
+  primaryWeapon: 5,
+  agility: 5,
+  hull: 5,
+  shield: 5,
+  upgrades: [
+    'proton',
+    'astromech'
+  ],
+  ability: 'My awesome ability',
+  actions: [
+    'focus',
+    'aquire'
+  ]
+};
